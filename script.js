9f065584f8551e9933037b6ce4eda1a201276de5
divzz0011 <divzzxxx@gmail.com>
2025-03-12 19:24:15 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function updateProfile() {
+    document.getElementById("name").textContent = "Divzz Updated";
+    document.getElementById("position").textContent = "Senior Software Engineer";
+    document.getElementById("email").textContent = "divzzxxx@gmail.com";
+    document.getElementById("phone").textContent = "+62 8181 8160 725";
+    document.getElementById("address").textContent = "Jl. ytta, Bandung";
+    document.getElementById("instagram").href = "https://instagram.com/@divzz_neverlouse";
+    document.getElementById("instagram").textContent = "@Divzz";
+}
